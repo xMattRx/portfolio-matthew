af15639865c98a82a73fc55f7708b407fbf12d45
Matthew Nascimento <matthew.nascimento@valtech.com>
2024-02-07 12:06:39 -0300

    experience section ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
     <header className={styles.header}>
-      <h1>Matthew R. R. Nascimento</h1>
+      <h1 className={styles.header__title}>Matthew R. R. Nascimento</h1>
       <nav>
         <ul className={styles.header__menu}>
           <li><a className={styles.header__link} href="#experiencia">Experiência</a></li>
@@ -17,13 +17,48 @@ export default function Home() {
       </nav>
     </header>
     
-    <main className={styles.introducao}>
-      <Image className={styles.introducao__image}  src={photo} alt="Matthew foto" />
+    <main className={styles.introduction}>
+      <Image  className={styles.introduction__image} width={360} height={520}  src={photo} alt="Matthew foto" />
       <div>
-        <h1 className={styles.introducao__title}>Desenvolvedor<br/> Front End</h1>
-        <p>Localizado em Belo Horizonte &#x1F306;</p>
+        <h1 className={styles.introduction__title}>Desenvolvedor<br/> Front End</h1>
+        <p className={styles.introduction__description}>Localizado em Belo Horizonte &#x1F306;</p>
       </div>
     </main>
+
+    <section className={styles.experience} id="experiencia" aria-label="Experiência">
+      <h2 className="subtitle" style={{color: "#e2e2e2"}}>Experiência</h2>
+      <div>
+          <p className={styles.experience__description}>Sou desenvolvedor <strong>Front-End</strong> na Valtech. Curso atualmente 
+            <strong> Engenharia de software</strong> na PUC Minas.</p>
+
+
+          <div className={styles.company}>
+            <span className={styles.company__year}>2021</span>
+            <h3 className={styles.company__title}>Compass.uol</h3>
+            <span className={styles.company__title}>Estágio</span>
+            <p className={styles.company__description}>Durante 4 meses, a empresa nos forneceu estrutura para aprendermos React JS e suas principais funcionais e bibliotecas. Como projeto final, desenvolvemos em equipe uma aplicação que é uma espécie de clone da Netflix utilizando as principais funcionalidades do React JS</p>
+            <ul className={styles.company__skills}>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>React JS</li>
+            </ul>
+          </div>
+          <div className={styles.company}>
+            <span className={styles.company__year}>2021</span>
+            <h3 className={styles.company__title}>Compass.uol</h3>
+            <span className={styles.company__title}>Estágio</span>
+            <p className={styles.company__description}>Durante 4 meses, a empresa nos forneceu estrutura para aprendermos React JS e suas principais funcionais e bibliotecas. Como projeto final, desenvolvemos em equipe uma aplicação que é uma espécie de clone da Netflix utilizando as principais funcionalidades do React JS</p>
+            <ul className={styles.company__skills}>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>React JS</li>
+            </ul>
+          </div>
+
+      </div>
+    </section>
 
     </>
     
