13a9b14c21bb27dd6ac3fe96e793bbd22a40ec90
Matthew Nascimento <matthew.nascimento@valtech.com>
2024-03-05 11:13:02 -0300

    feat: update yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Enable static export mode
-    output: 'export',
+    output:'export',
   };
-  
+  /* module.exports = nextConfig*/
   export default nextConfig;
